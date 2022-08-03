d1239d7dd895f1655cd7dbbc2bb15a68d2150806
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-08-03 18:24:34 +0900

    commit

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,12 +1,1 @@
 package helpers
-
-import (
-	"math/rand"
-	"time"
-)
-
-func RandomNumber(n int) int {
-	rand.Seed(time.Now().UnixNano())
-	value := rand.Intn(n)
-	return value
-}
--- main.go
+++ main.go
@@ -1,24 +1,69 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
-
-	"github.com/hiroshi-iwashita/udemy-buildingModernWebApplicationsWithGo/helpers"
 )
 
-const numPool = 100
-
-func CalculateValue(intChan chan int) {
-	randomNumber := helpers.RandomNumber(numPool)
-	intChan <- randomNumber
+type Person struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	HairColor string `json:"hair_color"`
+	HasDog    bool   `json:"has_dog"`
 }
 
 func main() {
-	intChan := make(chan int)
-	defer close(intChan)
 
-	go CalculateValue(intChan)
+	myJson := `
+		[
+			{
+				"first_name": "Clark",
+				"last_name": "Kent",
+				"hair_color": "black",
+				"has_dog": true
+			},
+			{
+				"first_name": "Bluce",
+				"last_name": "Wayne",
+				"hair_color": "black",
+				"has_dog": false
+			}
+		]
+	`
 
-	num := <-intChan
-	log.Println(num)
+	var unmarshalled []Person
+
+	err := json.Unmarshal([]byte(myJson), &unmarshalled)
+
+	if err != nil {
+		log.Println("Error unmarshalling json", err)
+	}
+	log.Printf("unmarshalled: %v", unmarshalled)
+
+	// write json from struct
+	var mySlice []Person
+
+	var m1 Person
+	m1.FirstName = "Wally"
+	m1.LastName = "West"
+	m1.HairColor = "red"
+	m1.HasDog = false
+
+	mySlice = append(mySlice, m1)
+
+	var m2 Person
+	m2.FirstName = "Diana"
+	m2.LastName = "Prince"
+	m2.HairColor = "black"
+	m2.HasDog = false
+
+	mySlice = append(mySlice, m2)
+
+	newJson, err := json.MarshalIndent(mySlice, "", "  ")
+
+	if err != nil {
+		log.Println("error marshaling", err)
+	}
+	fmt.Println(string(newJson))
 }
