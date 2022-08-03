63a857f6755a3efdc57c541c5e12b8750c0185d7
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-08-03 18:14:06 +0900

    commit

--- main.go
+++ main.go
@@ -2,6 +2,37 @@ package main
 
 import "fmt"
 
+var myName string
+
 func main() {
-	fmt.Println("Hello, world!")
+	fmt.Println("Hello world.")
+
+	var whatToSay string
+	var i int
+
+	whatToSay = "Goodbye cruel world"
+	fmt.Println(whatToSay)
+
+	i = 7
+	fmt.Println("i is set to", i)
+
+	whatWasSaid, theOtherThingThatWasSaid := saySomething()
+	fmt.Println("The function returned", whatWasSaid, "fsuhjsdf", theOtherThingThatWasSaid)
+
+	setMyName("Hiroshi")
+	name := getMyName()
+	fmt.Println(name)
+
+}
+
+func saySomething() (string, string) {
+	return "something", "else"
+}
+
+func setMyName(nameToSet string) {
+	myName = nameToSet
+}
+
+func getMyName() string {
+	return myName
 }
