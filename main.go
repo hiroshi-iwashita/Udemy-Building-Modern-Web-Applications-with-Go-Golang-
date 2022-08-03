f3d903a1f818e224a777a613788ee664d4f27506
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-08-03 18:15:39 +0900

    commit

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"time"
+)
+
+var s = "seven"
+
+type User struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         string
+	BirthDate   time.Timer
+}
+
+type myStruct struct {
+	FirstName string
+}
+
+func (m *myStruct) pringFirstName() string {
+	return m.FirstName
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+
+	var myVar myStruct
+	myVar.FirstName = "John"
+
+	myVar2 := myStruct{
+		FirstName: "Mary",
+	}
+
+	// log.Println("myVar is set to", myVar.FirstName)
+	// log.Println("myVar2 is set to", myVar2.FirstName)
+	log.Println("myVar is set to", myVar.pringFirstName())
+	log.Println("myVar2 is set to", myVar2.pringFirstName())
+
+	user := User{
+		FirstName: "Trevor",
+		LastName:  "Hiroshi",
+	}
+
+	log.Println(user.FirstName, user.LastName, "Birthdate:", user.BirthDate)
 }
