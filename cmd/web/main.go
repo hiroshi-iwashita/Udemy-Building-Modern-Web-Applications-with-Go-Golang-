058a78b0b2776bb0dbf064182a7a1da5d9a6d1fe
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-08-04 21:54:10 +0900

    commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/hiroshi-iwashita/udemy-building-modern-web-applications-with-go/pkg/config"
 	"github.com/hiroshi-iwashita/udemy-building-modern-web-applications-with-go/pkg/handlers"
+	"github.com/hiroshi-iwashita/udemy-building-modern-web-applications-with-go/pkg/render"
 )
 
 const portNumber = ":8080"
 
 // main is the main application function
 func main() {
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
 
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -10,8 +10,10 @@ import (
 
 // RenderTemplate renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	// get the template cache from the app config
+
 	// create a template cache
-	tc, err := createTemplateCache()
+	tc, err := CreateTemplateCache()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -36,7 +38,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	}
 }
 
-func createTemplateCache() (map[string]*template.Template, error) {
+func CreateTemplateCache() (map[string]*template.Template, error) {
 	// myCache := make(map[string]*template.Template)  // same as below
 	myCache := map[string]*template.Template{}
 
